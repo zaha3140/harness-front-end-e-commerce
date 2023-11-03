18be6b7a3dfa46f406111ba028661f7160565baf
zaha3140 <146467052+zaha3140@users.noreply.github.com>
2023-11-03 18:03:39 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,25 @@
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        }
+        int[] data = new int[10];
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+
+        for (int item : data) {
+public class RandomDataGenerator {
+    }
+
+}
+        for (int i = 0; i < 10; i++) {}
+
+
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+        }
+        }
+    }
+public class RandomDataGenerator {
+        for (int item : data) {
+        int[] data = new int[10];
